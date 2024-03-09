dd2ea7a7305cabd59353847d3ecea17ba87f81a7
tonyturtl <135500454+tonyturtl@users.noreply.github.com>
2024-03-09 12:58:46 -0500

    snake

--- Assets/Scenes/timer.cs
+++ Assets/Scenes/timer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class timer : MonoBehaviour
 {
-    public float timeLeft;
+    public float TimeLeft;
     public Text TimerText;
 
     // Start is called before the first frame update
@@ -16,15 +16,15 @@ public class timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (timeLeft > 0)
+       if (TimeLeft > 0)
        {
-        timeLeft -= Time.deltatime;
-        UpdateTimer(timeLeft);
+        TimeLeft -= Time.deltaTime;
+        UpdateTimer(TimeLeft);
        } 
        else
        {
         Debug.Log("Game Over");
-        timeLeft = 0;
+        TimeLeft = 0;
        }
     }
 
@@ -32,8 +32,10 @@ public class timer : MonoBehaviour
     {
         currentTime += 1;
 
-        float minutes = Mathf.FloorToInt (currentTime/60)
-        float seconds = Mathf.FloorToInt ()
+        float minutes = Mathf.FloorToInt (currentTime/60);
+        float seconds = Mathf.FloorToInt (currentTime%60);
+
+        TimerText.text = string.Format ("{0:00} : {1:00}" ,minutes, seconds);
     }
 
 }
